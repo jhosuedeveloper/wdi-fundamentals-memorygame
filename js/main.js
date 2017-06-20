600a7508de4563ff7bd34dcda8cdaf47bb4f30c5
josh <josh@joshs-iMac.home>
2017-06-20 16:14:43 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,25 +1,36 @@
- console.log("Up and running!");
+ //Name: JR
 
 
  var cards = ["queen", "queen", "king", "king"];
  var cardsInPlay = [];
- var cardOne = cards [0];
- cardsInPlay.push(cardOne);
- console.log("User flipped "+ cardsInPlay[0]);
 
- cardTwo = cards[2];
- cardsInPlay.push(cardTwo);
- console.log("User Flipped " + cardsInPlay[1]);
+var checkForMatch = function()
+{
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+  } else {
+      alert("Sorry, try again.");
+  }
+}
+
+
+
+ var flipCard = function(cardID) 
+ {
+
+console.log("User flipped " + cards[cardID]);
+cardsInPlay.push(cards[cardID]);
+
 
  if(cardsInPlay.length === 2)
  {
- 	if(cardsInPlay[0] === cardsInPlay[1])
- 	{
- 		alert("you found a match");
- 	}
- 	else
- 	{
- 		alert("Sorry, try again");
- 	}
-
+ 	checkForMatch();
  }
+
+
+
+}
+
+flipCard(0);
+flipCard(2);
+
