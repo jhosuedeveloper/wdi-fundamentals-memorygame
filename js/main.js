75187b930f08e7bd9eb35e13f79664aba4b4a6e8
josh <josh@joshs-iMac.home>
2017-06-21 08:04:55 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -26,11 +26,15 @@
  ];
 
  var cardsInPlay = [];
-
+var counter = 0;
+document.getElementById('numbers').innerHTML = counter;
 var checkForMatch = function()
+
 {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
       alert("You found a match!");
+      counter++;
+      appendingScores();
   } else {
       alert("Sorry, try again.");
   }
@@ -38,14 +42,19 @@ var checkForMatch = function()
 
 
 
- var flipCard = function(cardID) 
+ var flipCard = function() 
  {
 
-console.log("User flipped " + cards[cardID].rank);
-cardsInPlay.push(cards[cardID].rank);
-console.log(cards[cardID].cardImage);
-console.log(cards[cardID].suit);
+var cardId = this.getAttribute('data-id');
 
+
+
+console.log("User flipped " + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
+
+this.setAttribute('src', cards[cardId].cardImage)
 
  if(cardsInPlay.length === 2)
  {
@@ -56,6 +65,60 @@ console.log(cards[cardID].suit);
 
 }
 
-flipCard(0);
-flipCard(2);
+
+var createBoard= function()
+{
+	for(var i=0; i<cards.length; i++)
+	{
+		var cardElement = document.createElement('img'); 
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id',i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+
+	}
+
+
+}
+
+createBoard();
+
+var reset = document.getElementsByTagName('button')[0];
+
+
+var resetting= function()
+{
+	
+	for(var i=0; i<cards.length; i++)
+	{
+	var item = document.getElementById('game-board')
+	item.removeChild(item.childNodes[0]);
+	item.removeEventListener('click',flipCard);
+	cardsInPlay=[];
+
+	}
+
+
+
+	createBoard();
+}
+reset.addEventListener('click', resetting);
+
+
+
+
+
+var appendingScores = function()
+{
+document.getElementById('numbers').innerHTML = counter;
+}
+
+
+
+
+
+
+
+
+
 
